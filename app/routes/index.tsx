b96f6f0fcb97988511b0fbbba7177ef9294d1388
Tatiana <tatimb14@gmail.com>
2022-04-24 00:14:15 -0600

    feat: bg to posts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
+import { MdWorkOutline } from 'react-icons/md';
 
 export default function Index() {
   const user = useOptionalUser();
@@ -10,8 +11,8 @@ export default function Index() {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="backdrop-blur-sm bg-white/10 relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
 
-            <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-8xl">
+            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-32 lg:pt-32 lg:pb-32 ">
+              <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-8xl lg:text-8xl">
                 <span className="block uppercase text-white drop-shadow-md">
                   Tatiana Moreira
                 </span>
@@ -19,55 +20,40 @@ export default function Index() {
               <p className=" mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
                 Hi there! I'm Tati a software engineer in love with the web.
               </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
-                  <Link
-                    to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-                  >
-                    View Notes for {user.email}
-                  </Link>
-                ) : (
-                    <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                      <Link
-                        to="/join"
-                        className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-                      >
-                        Sign up
-                    </Link>
-                      <Link
-                        to="/login"
-                        className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600  "
-                      >
-                        Log In
-                    </Link>
+         
 
-                    </div>
-                  )}
-              </div>
-              
             </div>
 
           </div>
         </div>
-        <div className=" p-3 mt-8 backdrop-blur-sm bg-white/10 relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-        <div className="flex">
-                <svg
-                  className="w-6 h-6 text-white fill-current mr-1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512">
-                  <path
-                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
-                  >
-                  </path>
-                </svg>
-                <svg aria-hidden="true" className="octicon text-white octicon-mark-github fill-current" height="24" version="1.1" viewBox="0 0 16 16" width="24">
-                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z">
-                  </path>
-                </svg>
-              </div>
-        </div>
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 flex justify-center">
+          <div className="max-w-xl content-center p-3 mt-8 backdrop-blur-sm bg-white/10 relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+            <div className="flex">
+              <Link
+                to="/posts"
+                className="mr-3 underline"
+              >
+                <MdWorkOutline size='26px' className="text-white" />
+              </Link>
 
+              <svg
+                className="w-6 h-6 text-white fill-current mr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512">
+                <path
+                  d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                >
+                </path>
+              </svg>
+              <svg aria-hidden="true" className="octicon text-white octicon-mark-github fill-current" height="24" version="1.1" viewBox="0 0 16 16" width="24">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z">
+                </path>
+              </svg>
+
+
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -4,38 +4,54 @@ import { getPosts } from "~/models/post.server";
 
 
 type Post = {
-    slug: string;
-    title: string;
-  };
-  
-  type LoaderData = {
-    posts: Array<Post>;
-  };
+  slug: string;
+  title: string;
+};
+
+type LoaderData = {
+  posts: Array<Post>;
+};
 
 export const loader = async () => {
-    return json<LoaderData>({
-      posts: await getPosts(),
+  return json<LoaderData>({
+    posts: await getPosts(),
   });
 };
 
 export default function Posts() {
-    const { posts } = useLoaderData() as LoaderData;
-    console.log(posts);
-    return (
-      <main>
-        <h1>Posts</h1>
-        <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link
-              to={post.slug}
-              className="text-blue-600 underline"
-            >
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      </main>
-    );
-  }
+  const { posts } = useLoaderData() as LoaderData;
+  console.log(posts);
+  return (
+    <main className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 relative min-h-screen sm:flex sm:items-center sm:justify-center">
+      <div className="relative sm:pb-16 sm:pt-8">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="backdrop-blur-sm bg-white/10 relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-32 lg:pt-32 lg:pb-32 ">
+
+              <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-3xl lg:text-3xl">
+                <span className="block uppercase text-white drop-shadow-md">
+                  Posts
+                </span>
+                
+              </h1>
+              <p className="mx-auto mt-3 max-w-lg text-center text-xl text-white sm:max-w-3xl">A collection of my writtings </p>
+              <ul className="mt-5">
+                {posts.map((post) => (
+                  <li key={post.slug} className="mt-3">
+                    <Link
+                      to={post.slug}
+                      className="text-white underline"
+                    >
+                      {post.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
